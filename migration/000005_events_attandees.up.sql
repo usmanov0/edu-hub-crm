2bd61b3fc77dfdb39ad5a6a6fa3c90dd612fbb85
usmanov0 <azikusmanov882@gmail.com>
2024-04-17 18:26:03 +0500

    done :relationship between event

--- migration/000005_events_attandees.up.sql
+++ migration/000005_events_attandees.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "event_attendees" (
     "id" INT REFERENCES events(id),
     "student_id" INT REFERENCES users(id),
+    "event_id" INT REFERENCES events(id),
     PRIMARY KEY (id, student_id)
 );
